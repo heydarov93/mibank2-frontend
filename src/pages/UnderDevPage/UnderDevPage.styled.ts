c78d64ff0fc06d21cb5fbffa2ce0cddb87b52cdf
Volha Kalachova <v.kalachova@andersenlab.com>
2024-04-16 22:13:20 +0400

    MBL-56/feature: update page under development

--- src/pages/UnderDevPage/UnderDevPage.styled.ts
+++ src/pages/UnderDevPage/UnderDevPage.styled.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 
 export const UnderDevPageWrapper = styled(Box)(() => ({
   display: 'flex',
@@ -7,6 +7,73 @@ export const UnderDevPageWrapper = styled(Box)(() => ({
   height: '70vh',
 }));
 
-export const StyledBox = styled(Box)(({ theme: { spacing } }) => ({
-  padding: spacing(2),
+export const StyledBox = styled(Box)(({ theme: { spacing, breakpoints } }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: spacing(3),
+  svg: {
+    width: '62vw',
+    height: 'auto',
+  },
+
+  [breakpoints.up('md')]: {
+    svg: {
+      width: '42vw',
+      height: 'auto',
+    },
+  },
+
+  [breakpoints.up('xl')]: {
+    gap: spacing(4),
+    svg: {
+      width: '40vw',
+      height: 'auto',
+    },
+  },
 }));
+
+export const StyledTitleContainer = styled(Box)(
+  ({ theme: { spacing, breakpoints } }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: spacing(1.5),
+
+    [breakpoints.up('xs')]: {
+      maxWidth: '80%',
+    },
+
+    [breakpoints.up('md')]: {
+      maxWidth: '60%',
+    },
+
+    [breakpoints.up('xl')]: {},
+  }),
+);
+
+export const StyledTitle = styled(Typography)(({ theme: { breakpoints } }) => ({
+  fontSize: '22px',
+
+  [breakpoints.up('md')]: {
+    fontSize: '28px',
+  },
+
+  [breakpoints.up('xl')]: {
+    fontSize: '36px',
+  },
+}));
+
+export const StyledDescription = styled(Typography)(
+  ({ theme: { breakpoints, palette } }) => ({
+    color: palette.grey[500],
+
+    [breakpoints.up('md')]: {
+      fontSize: '16px',
+    },
+
+    [breakpoints.up('xl')]: {},
+  }),
+);
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,43 +1,54 @@
-import {createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
-import {ErrorPage, LoginPage, UnderDevPage} from '../pages';
+import { ErrorPage, LoginPage, UnderDevPage } from '../pages';
 
-import { PrivateRoute } from "./PrivateRoute";
+import { PrivateRoute } from './PrivateRoute';
 
-import { App } from "App";
+import { App } from 'App';
 
 const routes = createBrowserRouter([
-    {
+  {
+    path: '/',
+    element: (
+      <PrivateRoute>
+        <App />
+      </PrivateRoute>
+    ),
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: '/',
-        element: 
-        <PrivateRoute><App /></PrivateRoute>
-        ,
+        element: (
+          <PrivateRoute>
+            <App />
+          </PrivateRoute>
+        ),
         errorElement: <ErrorPage />,
         children: [
-            {
-                index: true,
-                element: <UnderDevPage />
-            },
-            {
-                path: '/payments',
-                element: <UnderDevPage />
-            },
-            {
-                path: '/transfers',
-                element: <UnderDevPage />
-            },
-            {
-                path: '/history',
-                element: <UnderDevPage />
-            }
-        ]
-    },
-    {
+          {
+            index: true,
+            element: <UnderDevPage />,
+          },
+          {
+            path: '/payments',
+            element: <UnderDevPage />,
+          },
+          {
+            path: '/transfers',
+            element: <UnderDevPage />,
+          },
+          {
+            path: '/history',
+            element: <UnderDevPage />,
+          },
+        ],
+      },
+      {
         path: '/signin',
-        element:
-        <LoginPage />
-    }
+        element: <LoginPage />,
+      },
+    ],
+  },
 ]);
 
-
-export {routes};
+export { routes };
