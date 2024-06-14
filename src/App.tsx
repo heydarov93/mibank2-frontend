4551096f54f9a710267fd32a7233fc42dd27535b
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-06-14 12:01:42 +0300

    refactor: move header folder into organisms

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Amplify } from 'aws-amplify';
 import { Outlet } from 'react-router-dom';
 
 import awsExports from './aws-exports';
-import { Header } from './components/Header';
+import { Header } from './components/organisms/Header';
 
 import { Footer } from 'components/organisms/Footer';
 
