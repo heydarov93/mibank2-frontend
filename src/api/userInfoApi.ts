e7f4b1948867592f2f880eb9ba0d2c747d10d77b
Andrei Chystabayeu <a.chystabayeu@andersenlab.com>
2024-06-17 19:51:29 +0000

    MLB-373/feature: log out from app

--- src/api/userInfoApi.ts
+++ src/api/userInfoApi.ts
@@ -6,16 +6,13 @@ import { endpoints } from './endpoints';
 
 import { baseQueryCreator } from 'store/baseQueryCreator';
 
-// TODO: remove once BE api is ready
-const mockEmail = 'jopoxemuxou-1783@yopmail.com';
-
 export const userInfoApi = createApi({
   reducerPath: 'userInfoApi',
   baseQuery: baseQueryCreator(),
   endpoints: (builder) => ({
-    getUserInfo: builder.query<IUserInfo, void>({
-      query: () =>
-        `${endpoints.userAccountManagement.users.userInformation}?email=${mockEmail}`,
+    getUserInfo: builder.query<IUserInfo, string | undefined>({
+      query: (email) => 
+        `${endpoints.userAccountManagement.users.userInformation}?email=${email}`,
     }),
   }),
 });
