e411d18ba3ef9641e06486cc382b4734cfb7800a
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-06-18 15:48:47 +0300

    MLB-331/style: update displaying time

--- src/utils/convertSecondsToTime.ts
+++ src/utils/convertSecondsToTime.ts
@@ -1,5 +1,5 @@
 export const convertSecondsToTime = (seconds: number) => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
-  return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+  return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
 };
