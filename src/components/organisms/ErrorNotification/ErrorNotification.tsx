e409803fd85b297624d8becab5561ff78335aff3
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-06-19 15:25:00 +0300

    MLB-425/feature: add title to error pop up

--- src/components/organisms/ErrorNotification/ErrorNotification.tsx
+++ src/components/organisms/ErrorNotification/ErrorNotification.tsx
@@ -1,4 +1,4 @@
-import { Alert, Snackbar } from '@mui/material';
+import { Alert, AlertTitle, Snackbar } from '@mui/material';
 
 import { useAppSelector, useAppDispatch } from 'hooks/hook';
 import { clearError } from 'store/reducers/AuthSlice';
@@ -8,6 +8,10 @@ export const ErrorNotification = () => {
   const dispatch = useAppDispatch();
   const error = useAppSelector(errorMessage);
   const open = Boolean(error);
+
+  const errorParts = error ? error.split('.') : [];
+  const title = errorParts.length > 1 ? errorParts[0] : '';
+  const message = errorParts.length > 1 ? errorParts.slice(1).join('.') : error;
 
   const handleClose = () => {
     dispatch(clearError());
@@ -19,9 +23,11 @@ export const ErrorNotification = () => {
       open={open}
       autoHideDuration={5000}
       onClose={handleClose}
+      sx={{ bgcolor: 'background.paper' }}
     >
-      <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-        <pre>{error}</pre>
+      <Alert onClose={handleClose} severity="error" color="error">
+        {message}
+        {title && <AlertTitle>{title}</AlertTitle>}
       </Alert>
     </Snackbar>
   );
