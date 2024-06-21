c329cc4e33f5637e8f026defd6b6e17749354468
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-06-21 16:46:38 +0300

    MLB-463/fix: add shouldForwardProp to StyledLogoContainer

--- src/components/atoms/Logo/Logo.styled.ts
+++ src/components/atoms/Logo/Logo.styled.ts
@@ -1,22 +1,22 @@
 import { Box, styled } from '@mui/material';
 
-export const StyledLogoContainer = styled(Box)<{ isMedium: boolean }>(
-  ({ theme: { spacing }, isMedium }) => ({
-    display: 'flex',
-    flexDirection: isMedium ? 'column' : 'row',
-    alignItems: 'center',
-    gap: spacing(1),
-  }),
-);
+export const StyledLogoContainer = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'isMedium',
+})<{ isMedium: boolean }>(({ theme: { spacing }, isMedium }) => ({
+  display: 'flex',
+  flexDirection: isMedium ? 'column' : 'row',
+  alignItems: 'center',
+  gap: spacing(1),
+}));
 
-export const StyledLogoNameContainer = styled(Box)<{ isSmall: boolean }>(
-  ({ theme: { palette }, isSmall }) => ({
-    color: palette.common.black,
-    ...(isSmall && {
-      maxWidth: '70px',
-    }),
+export const StyledLogoNameContainer = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'isSmall',
+})<{ isSmall: boolean }>(({ theme: { palette }, isSmall }) => ({
+  color: palette.common.black,
+  ...(isSmall && {
+    maxWidth: '70px',
   }),
-);
+}));
 
 export const StyledLogo = styled(Box, {
   shouldForwardProp: (prop) => prop !== 'isSmall',
