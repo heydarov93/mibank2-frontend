38ca9fc7144edcc09543a3cadfff337d6fb6102f
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-06-24 12:49:03 +0300

    MLB-463/fix: make title displayed first in error message

--- src/components/organisms/ErrorNotification/ErrorNotification.tsx
+++ src/components/organisms/ErrorNotification/ErrorNotification.tsx
@@ -47,8 +47,8 @@ export const ErrorNotification = ({
           color: theme.palette.error.main,
         }}
       >
-        {message}
         {title && <AlertTitle>{title}</AlertTitle>}
+        {message}
       </Alert>
     </Snackbar>
   );
