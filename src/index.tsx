7ffddacfead713f8a232bc341a6187bce90dff04
Andrei Chystabayeu <a.chystabayeu@andersenlab.com>
2024-06-25 08:18:49 +0000

    Mlb 467/bugfix/fix possibility log in with invalid credentials

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,12 @@ import ReactDOM from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import i18n from '../src/i18n';
 
 import reportWebVitals from './reportWebVitals';
 import { routes } from './router';
-import { persistor, store } from './store/store';
+import { store } from './store/store';
 import { theme } from './theme/theme';
 
 const root = ReactDOM.createRoot(
@@ -20,12 +19,10 @@ root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <ThemeProvider theme={theme}>
-            <RouterProvider router={routes} />
-            <CssBaseline />
-          </ThemeProvider>
-        </PersistGate>
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={routes} />
+          <CssBaseline />
+        </ThemeProvider>
       </Provider>
     </I18nextProvider>
   </React.StrictMode>,
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,9 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 import AuthReducer from './reducers/AuthSlice';
 
 import { authApi } from 'api/authApi';
 import { userInfoApi } from 'api/userInfoApi';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['auth'],
-};
 
 const rootReducer = combineReducers({
   auth: AuthReducer,
@@ -28,19 +11,11 @@ const rootReducer = combineReducers({
   [userInfoApi.reducerPath]: userInfoApi.reducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat([authApi.middleware, userInfoApi.middleware]),
+    getDefaultMiddleware().concat([authApi.middleware, userInfoApi.middleware]),
 });
-
-export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
