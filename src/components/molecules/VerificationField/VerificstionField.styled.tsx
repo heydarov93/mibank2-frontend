4ce8fdf4c9e50870c55f78953f3dca6489acd395
Anna Hetsman <a.hetsman@andersenlab.com>
2024-06-25 05:38:43 +0000

    MLB-418/feature: implement entering digits logic

--- src/components/molecules/VerificationField/VerificstionField.styled.tsx
+++ src/components/molecules/VerificationField/VerificstionField.styled.tsx
@@ -1,4 +1,28 @@
-import { styled, Input } from '@mui/material';
+import { styled, Input, keyframes, Box } from '@mui/material';
+
+const shakeAnimation = keyframes`
+  0% { transform: translateX(0); }
+  25% { transform: translateX(-5px); }
+  50% { transform: translateX(5px); }
+  75% { transform: translateX(-5px); }
+  100% { transform: translateX(0); }
+`;
+
+export const StyledVerificationBox = styled(Box)(
+  ({ theme: { palette, spacing } }) => ({
+    display: 'flex',
+    gap: spacing(1),
+    alignItems: 'center',
+
+    '&.shake': {
+      animation: `${shakeAnimation} 0.25s`,
+      '&>div': {
+        border: `1px solid ${palette.error.main}`,
+        backgroundColor: `${palette.error.light}`,
+      },
+    },
+  }),
+);
 
 export const StyledInputElement = styled(Input)(
   ({ theme: { palette, spacing } }) => ({
@@ -14,6 +38,10 @@ export const StyledInputElement = styled(Input)(
 
     '&.Mui-focused': {
       border: `2px solid ${palette.primary.main}`,
+    },
+
+    '&.hasValue': {
+      border: `1px solid ${palette.primary.main}`,
     },
   }),
 );
