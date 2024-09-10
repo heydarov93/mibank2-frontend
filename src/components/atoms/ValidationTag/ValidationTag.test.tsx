b5df297d4eefd4af95a0de662cfed3e808c8d341
Rakhman Mukeyev <r.mukeyev@andersenlab.com>
2024-09-10 05:29:24 +0000

    MLB-912/feature: create UI for password validation elements

---
+++ src/components/atoms/ValidationTag/ValidationTag.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+
+import { ValidationTag } from './ValidationTag';
+
+describe('ValidationTag', () => {
+  it('should render without crashing', () => {
+    const { container } = render(
+      <ValidationTag tagText={''} isValidated={true} />,
+    );
+    expect(container).toBeInTheDocument();
+  });
+
+  it('displays the correct text', () => {
+    render(<ValidationTag tagText="Minimum 8 characters" isValidated={true} />);
+    expect(screen.getByText('Minimum 8 characters')).toBeInTheDocument();
+  });
+
+  it('renders CheckIcon when isValidated is true', () => {
+    render(<ValidationTag tagText="" isValidated={true} />);
+    expect(screen.getByTestId('CheckIcon')).toBeInTheDocument();
+  });
+
+  it('renders ClearIcon when isValidated is false', () => {
+    render(<ValidationTag tagText="" isValidated={false} />);
+    expect(screen.getByTestId('ClearIcon')).toBeInTheDocument();
+  });
+});
