118f05669bc9fcbe8252202a608f10f8e5553f1f
Rakhman Mukeyev <r.mukeyev@andersenlab.com>
2024-10-10 12:36:32 +0000

    Mlb 1184/fix/some special chars not recognized in password field

--- src/validation/passwordValidationRules.ts
+++ src/validation/passwordValidationRules.ts
@@ -3,7 +3,7 @@ import { ValidationKey } from 'enums';
 export const passwordValidationRules = {
   [ValidationKey.MINIMUM_CHAR]: (v: string) => v.length >= 8,
   [ValidationKey.LATIN_LETTERS_ONLY]: (v: string) =>
-    /^[A-Za-z\d~!?@#$%^&*_\-+()[\]{}<>/|\\"'.,:;`=]+$/.test(v),
+    /^(?=.*[A-Za-z])[A-Za-z0-9~!?@#$%^&*_\-+()[\]{}<>/|\\"'.,:;`=]+$/.test(v),
   [ValidationKey.ONE_DIGIT]: (v: string) => /\d/.test(v),
   [ValidationKey.LETTER_IN_UPPERCASE]: (v: string) => /[A-Z]/.test(v),
   [ValidationKey.LETTER_IN_LOWERCASE]: (v: string) => /[a-z]/.test(v),
