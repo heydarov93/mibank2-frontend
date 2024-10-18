b11b346a6cf23f63b83a13073303ad655f8fe2c8
Uladzislau Poleshchuk <u.poleshchuk@andersenlab.com>
2024-10-18 14:03:16 +0000

    Mlb 1207/feature/create input fields for name and surname

--- src/models/IRegistration.ts
+++ src/models/IRegistration.ts
@@ -1,6 +1,6 @@
 export interface IPersonalInfo {
-  // name: string | undefined;
-  // surname: string | undefined;
+  name: string;
+  surname: string;
   // dateOfBirth: string | undefined;
   phoneNumber: number;
 }
