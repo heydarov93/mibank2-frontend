6edf9c79dda5497b2d39439e48e306cd5bca780d
Uladzislau Poleshchuk <u.poleshchuk@andersenlab.com>
2024-10-28 07:04:19 +0000

    Mlb 1206/feature/create input field for date of birth

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -5,3 +5,4 @@ export { UserCard } from './UserCard/UserCard';
 export { Timer } from './Timer/Timer';
 export { RegistrationSideBar } from './RegistrationSideBar/RegistrationSideBar';
 export { PhoneNumberField } from './PhoneNumberField/PhoneNumberField';
+export { DateOfBirthField } from './DateOfBirthField/DateOfBirthField';
