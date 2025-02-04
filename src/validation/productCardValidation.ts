0dc10e50161084fe88fdab33ed08351c574cc72c
Vishal Raj <v.raj@andersenlab.com>
2025-02-04 19:25:43 +0000

    MLB-1810/feature: created layout for adding card product

---
+++ src/validation/productCardValidation.ts
@@ -0,0 +1,48 @@
+import * as yup from 'yup';
+
+import i18n from 'i18n';
+
+export const productCardValidation = yup.object().shape({
+  cashbackRate: yup
+    .number()
+    .required(i18n.t('BackOffice.VisaCard.required'))
+    .typeError(i18n.t(`BackOffice.VisaCard.number`))
+    .test(
+      'decimal-places',
+      i18n.t(`BackOffice.VisaCard.decimal`),
+      (value) =>
+        value === undefined || /^\d+(\.\d{1,2})?$/.test(value.toString()),
+    ),
+  monthlyFee: yup
+    .number()
+    .required(i18n.t('BackOffice.VisaCard.required'))
+    .typeError(i18n.t(`BackOffice.VisaCard.number`))
+    .test(
+      'decimal-places',
+      i18n.t(`BackOffice.VisaCard.decimal`),
+      (value) =>
+        value === undefined || /^\d+(\.\d{1,2})?$/.test(value.toString()),
+    ),
+  dailyOperationalLimit: yup
+    .number()
+    .required(i18n.t('BackOffice.VisaCard.required'))
+    .typeError(i18n.t(`BackOffice.VisaCard.number`))
+    .test(
+      'decimal-places',
+      i18n.t(`BackOffice.VisaCard.decimal`),
+      (value) =>
+        value === undefined || /^\d+(\.\d{1,2})?$/.test(value.toString()),
+    ),
+  foreignTransactionLimit: yup
+    .number()
+    .required(i18n.t('BackOffice.VisaCard.required'))
+    .typeError(i18n.t(`BackOffice.VisaCard.number`))
+    .test(
+      'decimal-places',
+      i18n.t(`BackOffice.VisaCard.decimal`),
+      (value) =>
+        value === undefined || /^\d+(\.\d{1,2})?$/.test(value.toString()),
+    ),
+  cardIssuer: yup.string().required(i18n.t('BackOffice.VisaCard.required')),
+  cardType: yup.string().required(i18n.t('BackOffice.VisaCard.required')),
+});
