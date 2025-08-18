248cbaf949582804dd69dd6a37d68ac343d4e186
Yashar Heydarov <y.heydarov@andersenlab.com>
2025-08-18 13:37:07 +0000

    bugfix → develop: MLB-3683 “Continue” button overlaps footer in Legal Entity Address section at certain viewport sizes

--- src/components/layouts/BusinessLoginLayout/BusinessLoginLayout.styled.ts
+++ src/components/layouts/BusinessLoginLayout/BusinessLoginLayout.styled.ts
@@ -1,9 +1,9 @@
-import Box from "@mui/material/Box";
-import { styled } from "@mui/material/styles";
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 
 export const StyledContainer = styled(Box)(() => ({
   display: 'flex',
   flexDirection: 'row',
-  height: '100vh',
+  minHeight: '100vh',
   width: '100%',
 }));
